81d60454ce2823f9d6cfd1366f9bd741b0576fd5
Ryan Davies <ryscot@gmail.com>
2016-03-29 13:38:06 -0700

    Init Commit

---
+++ src/main/java/Vote.java
@@ -0,0 +1,74 @@
+import static spark.Spark.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import org.json.simple.*;
+
+@SuppressWarnings("unchecked")
+public class Vote {
+  private static final String STATIC_FILE_LOCATION = "/public";
+
+  @SuppressWarnings("serial")
+  private static final Map<String, Integer> candVotes = new HashMap<String, Integer>() {{
+    put("trump", 0);
+    put("hillary", 0);
+    put("kasich", 0);
+  }};
+
+  @SuppressWarnings("serial")
+  public static void main(String[] args) {
+
+    Spark.staticFileLocation(STATIC_FILE_LOCATION);
+
+
+    get("/", (req, res) -> new ModelAndView(candVotes, "index.mustache"), new MustacheTemplateEngine());
+
+    get("/votes", (req, res) -> {
+      JSONObject jsonObj = new JSONObject() {{
+        putAll(candVotes);
+      }};
+
+      return jsonObj.toJSONString();
+    });
+
+    get("/votes/:name", (req, res) -> {
+      String name = req.params(":name");
+
+      Integer numVotes = candVotes.get(name);
+
+      if (numVotes == null) {
+        halt(404, "Candidate " + name + " cannot be voted on.");
+      }
+
+      JSONObject jsonObj = new JSONObject() {{
+        put(name, numVotes);
+      }};
+
+      return jsonObj.toJSONString();
+    });
+
+    post("/voter/:name", (req, res) -> {
+      String name = req.params(":name");
+
+      if (!candVotes.containsKey(name)) {
+        halt(404, "Candidate " + name + " cannot be voted on.");
+      }
+
+      // Increment Vote
+      candVotes.put(name, candVotes.get(name) + 1);
+
+      JSONObject jsonObj = new JSONObject() {{
+        put(name, candVotes.get(name));
+      }};
+
+      res.redirect("/");
+
+      return jsonObj.toJSONString();
+    });
+  }
+}
